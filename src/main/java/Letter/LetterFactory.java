a781a0d83446aea9819e15b016f83bc615b4e690
naji-ba <122920174+naji-ba@users.noreply.github.com>
2024-02-05 23:52:02 -0500

    Version Finale

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -27,17 +27,21 @@ public final class LetterFactory {
     public static BaseShape create_A()  {
         Collection<Point2d> lettreA = new ArrayList<>();
 
-        Ellipse ellipseGauche = new Ellipse(4.0, 4.0);
-        ellipseGauche.translate(ellipseGauche.cloneCoords(),new Point2d(0.0, 2.0));
-        lettreA.addAll(ellipseGauche.cloneCoords());
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
+        lettreA.addAll(barreVerticale.cloneCoords());
 
-        Ellipse ellipseDroite = new Ellipse(4.0, 4.0);
-        ellipseDroite.translate(ellipseDroite.cloneCoords(),new Point2d(8.0, 2.0));
-        lettreA.addAll(ellipseDroite.cloneCoords());
+        Rectangle barreDroite = new Rectangle(0.5, 32.0);
+        barreDroite.translate(barreDroite.getCoords(), new Point2d(10.0, 0.0));
+        lettreA.addAll(barreDroite.getCoords());
 
-        Rectangle barreHorizontale = new Rectangle(8.0, 1.0);
-        barreHorizontale.translate(barreHorizontale.cloneCoords(),new Point2d(0.0,4.0));
-        lettreA.addAll(barreHorizontale.cloneCoords());
+
+        Rectangle barreMilieu = new Rectangle(10.0, 1.0);
+        barreMilieu.translate(barreMilieu.getCoords(), new Point2d(5.0, 0.0));
+        lettreA.addAll(barreMilieu.getCoords());
+
+        Rectangle barreHaut = new Rectangle(10.0, 1.0);
+        barreHaut.translate(barreHaut.getCoords(), new Point2d(5.0, -16.0));
+        lettreA.addAll(barreHaut.getCoords());
 
         return new BaseShape(lettreA);
     }
@@ -50,18 +54,25 @@ public final class LetterFactory {
 
         Collection<Point2d> lettreB = new ArrayList<>();
 
-
-        Ellipse ellipseSup = new Ellipse(6.0, 6.0);
-        ellipseSup.translate(ellipseSup.cloneCoords(), new Point2d(4.0, 6.0));
-        lettreB.addAll(ellipseSup.cloneCoords());
-
-        Ellipse ellipseInf = new Ellipse(6.0, 6.0);
-        ellipseInf.translate(ellipseInf.cloneCoords(), new Point2d(4.0, 0.0));
-        lettreB.addAll(ellipseInf.cloneCoords());
-
-        Rectangle barreVerticale = new Rectangle(2.0, 12.0);
-        barreVerticale.translate(barreVerticale.cloneCoords(),new Point2d(8.0,0.0));
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
         lettreB.addAll(barreVerticale.cloneCoords());
+
+        Rectangle barreDroite = new Rectangle(0.5, 32.0);
+        barreDroite.translate(barreDroite.getCoords(), new Point2d(10.0, 0.0));
+        lettreB.addAll(barreDroite.getCoords());
+
+
+        Rectangle barreMilieu = new Rectangle(10.0, 1.0);
+        barreMilieu.translate(barreMilieu.getCoords(), new Point2d(5.0, 0.0));
+        lettreB.addAll(barreMilieu.getCoords());
+
+        Rectangle barreHaut = new Rectangle(10.0, 1.0);
+        barreHaut.translate(barreHaut.getCoords(), new Point2d(5.0, -16.0));
+        lettreB.addAll(barreHaut.getCoords());
+
+        Rectangle barreBas = new Rectangle(10.0, 1.0);
+        barreBas.translate(barreBas.getCoords(), new Point2d(5.0, 16.0));
+        lettreB.addAll(barreBas.getCoords());
 
         return new BaseShape(lettreB);
     }
@@ -73,9 +84,16 @@ public final class LetterFactory {
 
         Collection<Point2d> lettreC = new ArrayList<>();
 
-        Ellipse ellipseC = new Ellipse(6.0, 12.0);
-        ellipseC.translate(ellipseC.cloneCoords(), new Point2d(6.0, 6.0));
-        lettreC.addAll(ellipseC.cloneCoords());
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
+        lettreC.addAll(barreVerticale.cloneCoords());
+
+        Rectangle barreHaut = new Rectangle(10.0, 1.0);
+        barreHaut.translate(barreHaut.getCoords(), new Point2d(5.0, -16.0));
+        lettreC.addAll(barreHaut.getCoords());
+
+        Rectangle barreBas = new Rectangle(10.0, 1.0);
+        barreBas.translate(barreBas.getCoords(), new Point2d(5.0, 16.0));
+        lettreC.addAll(barreBas.getCoords());
 
         return new BaseShape(lettreC);
     }
@@ -86,23 +104,21 @@ public final class LetterFactory {
      */
     public static BaseShape create_E() {
         Collection<Point2d> lettreE = new ArrayList<>();
-        // on veut faire 3 barres horizontales, Haut, milieu et bas, et une barre verticale
-        Rectangle barreSup = new Rectangle(10.0, 2.0);
-        barreSup.translate(barreSup.getCoords(), new Point2d(0.0, 6.0));
-        lettreE.addAll(barreSup.getCoords());
 
-        Rectangle barreMilieu = new Rectangle(10.0, 2.0);
-        barreMilieu.translate(barreMilieu.getCoords(), new Point2d(0.0, 6.0));
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
+        lettreE.addAll(barreVerticale.cloneCoords());
+
+        Rectangle barreMilieu = new Rectangle(10.0, 1.0);
+        barreMilieu.translate(barreMilieu.getCoords(), new Point2d(5.0, 0.0));
         lettreE.addAll(barreMilieu.getCoords());
 
-        Rectangle barreInf = new Rectangle(2.0, 12.0);
-        barreInf.translate(barreInf.getCoords(), new Point2d(8.0, 0.0));
-        lettreE.addAll(barreInf.getCoords());
+        Rectangle barreHaut = new Rectangle(10.0, 1.0);
+        barreMilieu.translate(barreHaut.getCoords(), new Point2d(5.0, -16.0));
+        lettreE.addAll(barreHaut.getCoords());
 
-        Rectangle barreVerticale = new Rectangle(2.0, 12.0);
-        barreVerticale.translate(barreVerticale.getCoords(), new Point2d(8.0, 0.0));
-        lettreE.addAll(barreVerticale.getCoords());
-
+        Rectangle barreBas = new Rectangle(10.0, 1.0);
+        barreBas.translate(barreBas.getCoords(), new Point2d(5.0, 16.0));
+        lettreE.addAll(barreBas.getCoords());
 
         return new BaseShape(lettreE);
     }
@@ -114,17 +130,16 @@ public final class LetterFactory {
     public static BaseShape create_H() {
         Collection<Point2d> lettreH = new ArrayList<>();
 
-        Rectangle barreGauche = new Rectangle(2.0, 12.0);
-        barreGauche.translate(barreGauche.getCoords(), new Point2d(0.0, 0.0));
-        lettreH.addAll(barreGauche.getCoords());
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
+        lettreH.addAll(barreVerticale.cloneCoords());
 
-        Rectangle barreDroite = new Rectangle(2.0, 12.0);
-        barreDroite.translate(barreDroite.getCoords(), new Point2d(8.0, 0.0));
+        Rectangle barreMilieu = new Rectangle(10.0, 1.0);
+        barreMilieu.translate(barreMilieu.getCoords(), new Point2d(5.0, 0.0));
+        lettreH.addAll(barreMilieu.getCoords());
+
+        Rectangle barreDroite = new Rectangle(0.5, 32.0);
+        barreDroite.translate(barreDroite.getCoords(), new Point2d(10.0, 0.0));
         lettreH.addAll(barreDroite.getCoords());
-
-        Rectangle barreHorizontale = new Rectangle(10.0, 2.0);
-        barreHorizontale.translate(barreHorizontale.getCoords(), new Point2d(0.0, 6.0));
-        lettreH.addAll(barreHorizontale.getCoords());
 
         return new BaseShape(lettreH);
     }
@@ -136,21 +151,18 @@ public final class LetterFactory {
     public static BaseShape create_N() {
         Collection<Point2d> lettreN = new ArrayList<>();
 
-        //barre verticale gauche
-        Rectangle barreGauche = new Rectangle(2.0, 12.0);
-        barreGauche.translate(barreGauche.getCoords(), new Point2d(0.0, 0.0));
-        lettreN.addAll(barreGauche.getCoords());
+        Rectangle barreVerticale = new Rectangle(0.5, 32.0);
+        lettreN.addAll(barreVerticale.cloneCoords());
+
+        Rectangle barreDroite = new Rectangle(0.5, 32.0);
+        barreDroite.translate(barreDroite.getCoords(), new Point2d(10.0, 0.0));
+        lettreN.addAll(barreDroite.getCoords());
 
         //barre diagonale
-        Rectangle barreDiagonale = new Rectangle(14.0, 2.0);
-        barreDiagonale.rotate(barreDiagonale.getCoords(), Math.toRadians(-45.0));
-        barreDiagonale.translate(barreDiagonale.getCoords(), new Point2d(0.0, 6.0));
+        Rectangle barreDiagonale = new Rectangle(10.0, 1.0);
+        barreDiagonale.rotate(barreDiagonale.getCoords(), Math.toRadians(-50.0));
+        barreDiagonale.translate(barreDiagonale.getCoords(), new Point2d(2.0, 8.0));
         lettreN.addAll(barreDiagonale.getCoords());
-
-        // barre verticale droite
-        Rectangle barreHorizontale = new Rectangle(10.0, 2.0);
-        barreHorizontale.translate(barreHorizontale.getCoords(), new Point2d(0.0, 6.0));
-        lettreN.addAll(barreHorizontale.getCoords());
 
         return new BaseShape(lettreN);
     }
@@ -162,9 +174,13 @@ public final class LetterFactory {
     public static BaseShape create_O() {
         Collection<Point2d> lettreO = new ArrayList<>();
 
-        Ellipse ellipseO = new Ellipse(6.0, 12.0);
-        ellipseO.translate(ellipseO.cloneCoords(), new Point2d(6.0, 6.0));
-        lettreO.addAll(ellipseO.cloneCoords());
+        //ellipse exterieure
+        Ellipse countour = new Ellipse(12.0, 32.0);
+        lettreO.addAll(countour.cloneCoords());
+
+        // ellipse interieure (creuse)
+        Ellipse interieure = new Ellipse(4.0,10.0);
+        lettreO.removeAll(interieure.cloneCoords());
 
         return new BaseShape(lettreO);
     }
